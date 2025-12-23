76f87d16ba990d3ed8ce05a6bbedd2712b9e7f6f
Khaled Fouad <khaledfouad687@gmail.com>
2025-12-23 10:32:15 +0200

    bas bage

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,41 +1,43 @@
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends  BaseTest {
-
-    @Test(dataProviderClass = TestProvider.class, dataProvider = "loginData")
-    public void validLoginTest(String username, String password) {
+    @Severity(SeverityLevel.CRITICAL)
+    @Test(priority = 1 ,  description = "Valid Login Test")
+    public void validLoginTest() {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login(username, password);
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html", "Login failed -); URL mismatch");
+        loginPage.login((String) jsonFileManager.getValue("username"),
+                        (String) jsonFileManager.getValue("password"));
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html",
+                "Login failed -); URL mismatch");
     }
 
-    @Test(dataProviderClass = TestProvider.class, dataProvider = "addToCartData")
-    public void addItemToCart(String username, String password, String itemId) {
-
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.login(username, password);
+  @Test
+//  (dataProviderClass = TestProvider.class, dataProvider = "addToCartData")
+    public void addItemToCart() {
+      LoginPage loginPage = new LoginPage(driver);
+      loginPage.login((String) jsonFileManager.getValue("username"),
+              (String) jsonFileManager.getValue("password"));
         ProductPage productPage = new ProductPage(driver);
-        productPage.clickProductAddToCart(itemId);
-
+        productPage.clickProductAddToCart(excelFileManager.getCellData(2,0)   );
         Assert.assertEquals(
              productPage.getProductPageTitle(), "Your Cart", "Add to cart failed -);"
         );
     }
 
-    @Test(dataProviderClass = TestProvider.class, dataProvider = "checkoutData")
-    public void checkout(String username, String password, String firstName, String lastName, String postalCode) {
+    @Test
+//            (dataProviderClass = TestProvider.class, dataProvider = "checkoutData")
+    public void checkout(String firstName, String lastName, String postalCode) {
      ProductPage productPage = new ProductPage(driver);
-
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login(username, password);
-
-        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
-        driver.findElement(By.className("shopping_cart_link")).click();
-        driver.findElement(By.id("checkout")).click();
         CheckOutPage checkOutPage = new CheckOutPage(driver);
-        checkOutPage.checkOut(firstName, lastName, postalCode);
+        loginPage.login((String) jsonFileManager.getValue("username"),
+                (String) jsonFileManager.getValue("password"));
+        productPage.clickProductAddToCart(excelFileManager.getCellData(  2,0)  );
+        checkOutPage.checkOut( firstName, lastName, postalCode );
         Assert. assertEquals(
            driver.findElement(By.className("complete-header")).getText(), "Thank you for your order!", "Checkout failed -);");
     }
